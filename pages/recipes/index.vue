fbaaf3347329fc7482e7c978f167b2ec75a8adb4
AmandaApril <68797138+AmandaApril@users.noreply.github.com>
2021-07-07 15:19:03 +0700

    nuxtjs

---
+++ pages/recipes/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="recipes">
+        <article class="recipe">
+            <div></div>
+            <h1>Title</h1>
+            <p>Some nice preview text</p>
+        </article>
+    </section>
+</template>
+<script>
+    
+</script>
+<style>
+    .recipes {
+        display:flex;
+        flex-flow: row wrap;
+    &rbrace;
+
+    .recipe {
+        box-siziing: border-box;
+        width: 280px;
+        padding: 8px;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px #aaa;
+    &rbrace;
+</style>
